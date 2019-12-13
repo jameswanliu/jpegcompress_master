e0f392a46e6bacfcb80ad2296bf7f35c567ef5d6
jameswanliu <952121091@qq.com>
2019-12-13 15:19:26 +0800

    添加图片压缩

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -12,10 +12,8 @@ extern "C" {
 }
 #define TAG "My_compress"
 #define LOG_D(...) __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
-
 typedef uint8_t BYTE;
-
-char error;
+char *error;
 
 int
 generatJPEG(BYTE *addstr, jint w, jint h, const char *name, jint quality, jboolean opmized);//定义方法
@@ -56,6 +54,23 @@ Java_com_james_jpegcompress_MainActivity_stringFromJNI(
 }
 
 
+/**
+ * 自定义 错误退出函数
+ * @param cinfo
+ */
+METHODDEF(void)
+my_error_exit(j_common_ptr cinfo) {
+    myerror myerr = (myerror) cinfo->err;
+    (*cinfo->err->output_message)(cinfo);//函数指针调用
+    error = const_cast<char *>(myerr->jmgr.jpeg_message_table[myerr->jmgr.msg_code]);
+    LOG_D("jpeg_message_table[%d]:%s", myerr->jmgr.msg_code,
+          myerr->jmgr.jpeg_message_table[myerr->jmgr.msg_code]);
+    // LOGE("addon_message_table:%s", myerr->pub.addon_message_table);
+//  LOGE("SIZEOF:%d",myerr->pub.msg_parm.i[0]);
+//  LOGE("sizeof:%d",myerr->pub.msg_parm.i[1]);
+    longjmp(myerr->jumpBuffer, 1);
+}
+
 
 /**
  *
@@ -66,32 +81,130 @@ Java_com_james_jpegcompress_MainActivity_stringFromJNI(
  */
 extern "C" JNIEXPORT jint JNICALL
 Java_com_james_jpegcompress_MainActivity_compressJpeg(
-        JNIEnv *env,
+        JNIEnv *env, jobject jobj,
         jobject bitmap, jint width, jint height, jint quality, jbyteArray bytearray,
         jboolean optimize) {
-    char * filename = jstringToString(env,bytearray);
+    char *filename = jstringToString(env, bytearray);
 
-    char *addptr = nullptr;
+    BYTE *addptr = nullptr;
 
     //获取bitmap info 以及锁住bitmap 像素
     AndroidBitmapInfo info;
-    if(AndroidBitmap_getInfo(env,bitmap,&info)<0){
+    if (AndroidBitmap_getInfo(env, bitmap, &info) < 0) {
         LOG_D("GET BITMAP INFO ERROR");
         return 0;
     }
-    if(AndroidBitmap_lockPixels(env,bitmap,(void**)addptr)<0){
+    if (AndroidBitmap_lockPixels(env, bitmap, reinterpret_cast<void **>(&addptr)) < 0) {
         LOG_D("LOCK BITMAP ERROR");
         return 0;
     }
 
 
+    BYTE *data;//存储每个像素rgb的指针
+    BYTE r, g, b;
+    BYTE *tempale;//临时bitmap 指针变量
+    data = static_cast<BYTE *> (malloc(width * height * 3));
+    int color;
+    tempale = data;
+    int i, j;
+    for (i = 0; i < height; i++) {// 行高遍历
+        for (j = 0; j < width; j++) {
+            color = *((int *) addptr);
+            r = ((color & 0x00FF0000) >> 16);
+            g = ((color & 0x0000FF00) >> 8);
+            b = color & 0x000000FF;
+
+            *data = b;//低位存储b
+            *(data + 1) = g;//移到下一个字节后存储g
+            *(data + 2) = r;//高位存储r
+
+            data = data + 3;//存储完一个像素的rgb后 移动到下一个存储位置
+            addptr += 4;//存储bitmap信息的指针移动到一个像素
+        }
+    }
 
 
-
-
-
+    AndroidBitmap_unlockPixels(env, bitmap);
+    int resultCode = generatJPEG(tempale, width, height, filename, quality, optimize);
+    if (resultCode < 0) {
+        LOG_D("generatJpeg error");
+        return 0;
+    }
+    free(tempale);
     return 1;
 }
 
 
+int generatJPEG(BYTE *data, jint w, jint h, const char *name, jint quality, jboolean opmized) {
+
+    int mComponent = 3;//rgb
+
+    struct jpeg_compress_struct jcs;
+
+    struct myErrorMsr jem;
+
+    jcs.err = jpeg_std_error(
+            &jem.jmgr);//cinfo.err = jpeg_std_error(&jerr);  然后是将错误处理结构对象绑定在JPEG对象上。
+    jem.jmgr.error_exit = my_error_exit;//自定义错误退出函数
+
+
+
+    /**
+     * goto语句看起来是处理异常的更可行方案。不幸的是，goto是本地的：它只能跳到所在函数内部的标号上，而不能将控制权转移到所在程序的任意地点（当然，除非你的所有代码都在main体中）。
+　　为了解决这个限制，C函数库提供了setjmp()和longjmp()函数，它们分别承担非局部标号和goto作用。头文件<setjmp.h>申明了这些函数及同时所需的jmp_buf数据类型。
+　　原理非常简单：
+　　l setjmp(j)设置“jump”点，用正确的程序上下文填充jmp_buf对象j。这个上下文包括程序存放位置、栈和框架指针，其它重要的寄存器和内存数据。当初始化完jump的上下文，setjmp()返回0值。
+　　l 以后调用longjmp(j,r)的效果就是一个非局部的goto或“长跳转”到由j描述的上下文处（也就是到那原来设置j的setjmp()处）。当作为长跳转的目标而被调用时，setjmp()返回r或1（如果r设为0的话）。（记住，setjmp()不能在这种情况时返回0。）
+     */
+    if (setjmp(jem.jumpBuffer)) {
+        return 0;
+    }
+    jpeg_create_compress(&jcs);//创建jpeg压缩
+    FILE *f = fopen(name, "wb");
+    if (f == NULL) {
+        return 0;
+    }
+    jpeg_stdio_dest(&jcs, f);//打开f的标准输出流
+    jcs.image_width = w;
+    jcs.image_height = h;
+
+    if (opmized) {
+        LOG_D("optimize==ture");
+    } else {
+        LOG_D("optimize==false");
+    }
+    jcs.arith_code = false;
+    //   jcs.num_components = mComponent;//num_components	/* # of color components in JPEG image */ jpeg 图片中的颜色分量 不能使用这个参数
+    jcs.input_components = mComponent; //input_components   of color components in input image 输入图像的颜色成分,既是 从每个像素中输入的rgb 分量
+    if (mComponent == 1)
+        jcs.in_color_space = JCS_GRAYSCALE;
+    else
+        jcs.in_color_space = JCS_RGB;
+
+    jpeg_set_defaults(&jcs);
+    jcs.optimize_coding = opmized;
+    jpeg_set_quality(&jcs, quality, true);
+    jpeg_start_compress(&jcs, TRUE);
+
+    JSAMPROW row_pointer[1];//一行像素样本图像 的 指针数组
+    int row_stride;
+    row_stride = jcs.image_width * mComponent;//图片的宽度 乘以 每个像素的所占字节（RGB）
+    while (jcs.next_scanline < jcs.image_height) {//如果下一行小于 图片的高则往下执行
+        row_pointer[0] = &data[jcs.next_scanline * row_stride];
+
+        jpeg_write_scanlines(&jcs, row_pointer, 1);//写入一行
+    }
+
+    if (jcs.optimize_coding) {
+        LOG_D("optimize==ture");
+    } else {
+        LOG_D("optimize==false");
+    }
+
+    jpeg_finish_compress(&jcs);
+    jpeg_destroy_compress(&jcs);
+
+    fclose(f);
+    return 1;
+}
 
--- app/src/main/java/com/james/jpegcompress/MainActivity.kt
+++ app/src/main/java/com/james/jpegcompress/MainActivity.kt
@@ -1,19 +1,109 @@
 package com.james.jpegcompress
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var path = ""
+    private val DEFAULT_QUALITY = 40
+    private val READ_EXTERNAL_STORAGE = 0
+    private val WRITE_EXTERNAL_STORAGE = 1
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        path = getExternalFilesDir("")?.canonicalPath + "/compress.jpg"
+        if (PermissionChecker.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) < 0
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_EXTERNAL_STORAGE
+            )
+        }
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+
+        if (PermissionChecker.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            < 0
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                WRITE_EXTERNAL_STORAGE
+            )
+        } else {
+            val file = File(path)
+            file.exists().apply {
+                file.delete()
+                file.createNewFile()
+            }
+        }
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.timg)
+
+        var ret = 0
+        iv_normal.setImageBitmap(bitmap)
+        btn_compress.setOnClickListener {
+            ret = compress(
+                bitmap, path
+            )
+            if (ret == 1) {
+                val comress = BitmapFactory.decodeFile(path)
+                iv_compress.setImageBitmap(comress)
+            }
+
+        }
     }
+
+
+    private fun sizeOf(value: Bitmap): Int {
+        return value.rowBytes * value.height
+    }
+
+
+    private fun compress(bitmap: Bitmap, fileName: String): Int {
+        return compressBitmap(bitmap, DEFAULT_QUALITY, fileName)
+    }
+
+    private fun compressBitmap(bit: Bitmap, quality: Int, fileName: String): Int {
+        var result: Bitmap? = null
+        try {
+            result = Bitmap.createBitmap(bit.width, bit.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(result)
+            val rect = Rect(0, 0, bit.width, bit.height)// original
+            canvas.drawBitmap(bit, null, rect, null)
+            return saveBitmap(result, quality, fileName)
+        } finally {
+            result?.recycle()
+        }
+    }
+
+
+    fun saveBitmap(bit: Bitmap, quality: Int, fileName: String): Int {
+        return compressJpeg(bit, bit.width, bit.height, quality, fileName.toByteArray(), true)
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -21,10 +111,15 @@ class MainActivity : AppCompatActivity() {
      */
     external fun stringFromJNI(): String
 
+
+    /**
+     * JNIEnv *env, jobject type, jobject bit, jint w, jint h, jint quality,
+    jbyteArray fileNameBytes_, jboolean optimize
+     */
     external fun compressJpeg(
-        bit: Bitmap, w: Int, h: Int, quality: Int, fileNameBytes: Array<Byte>,
+        bit: Bitmap, w: Int, h: Int, quality: Int, fileNameBytes: ByteArray,
         optimize: Boolean
-    ):Int
+    ): Int
 
 
     companion object {
@@ -33,5 +128,25 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("native-lib")
         }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults.size != 1 || grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val file = File(path)
+            file.exists().apply {
+                file.delete()
+                file.createNewFile()
+            }
+        } else {
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+
     }
 }
